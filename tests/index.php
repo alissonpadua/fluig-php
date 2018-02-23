ae38c64b7fc089a3d30b8cac10add12a3de5701a
= <=>
2018-02-23 17:38:30 -0300

    Dataset constrainst implementation

--- tests/index.php
+++ tests/index.php
@@ -4,6 +4,10 @@ require __DIR__. '/../vendor/autoload.php';
 
 $env = new \Symfony\Component\Dotenv\Dotenv;
 
+/* env test
 $env->load(__DIR__. '/../.env');
-
 echo getenv('FLUIG_CONSUMER_KEY');
+*/
+
+$cons = new \AlissonPadua\Model\Constraint;
+$cons->setType('MUST');
