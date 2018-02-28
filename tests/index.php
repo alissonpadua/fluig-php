dbc83e1732779145bfc7f3096637fdd5817b6f80
= <=>
2018-02-28 11:31:50 -0300

    Clear test file

--- tests/index.php
+++ tests/index.php
@@ -2,21 +2,10 @@
 
 require __DIR__. '/../vendor/autoload.php';
 
-/*$env = new \Symfony\Component\Dotenv\Dotenv;
-$env->load(__DIR__. '/../.env');
-echo getenv('FLUIG_CONSUMER_KEY');
-*/
-
-/*
-$dataset = new \AlissonPadua\Model\Dataset;
-$c1 = new \AlissonPadua\Model\Constraint('nome_colaborador', '100', '200', 'MUST');
-$dataset->addConstraint($c1);
-var_dump($dataset->getListConstraints());*/
-
 $dataset = new \AlissonPadua\PhpFluig\Model\Dataset;
 $datasetService = new \AlissonPadua\PhpFluig\Service\DatasetService;
 
-$dataset->setName("buscarTodosCooperados");
+$dataset->setName("colleague");
 
 $json = $datasetService->getDataset($dataset);
 
